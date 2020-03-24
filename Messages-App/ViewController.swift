86e5b45c3f8bfbefe727caa6c190cfe850a0dde0
Dmitrii Konstantinov <CasperNEW@inbox.ru>
2020-03-24 08:32:49 +0300

    init

---
+++ Messages-App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Messages-App
+//
+//  Created by Дмитрий Константинов on 24.03.2020.
+//  Copyright © 2020 Дмитрий Константинов. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
